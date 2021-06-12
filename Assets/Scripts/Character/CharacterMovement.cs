21760f821fc99223a54fb09e5c55a76e7b0a1183
erik <erik@pristimantis.com>
2021-06-12 16:24:48 +0200

    Working movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Pathfinding.Poly2Tri;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float m_Speed;
+
+    private Coroutine m_MovementCoroutine;
+    
+    public void OnMovement(InputAction.CallbackContext input)
+    {
+        if (input.phase == InputActionPhase.Started)
+        {
+            if (m_MovementCoroutine != null)
+            {
+                StopCoroutine(m_MovementCoroutine);
+            }
+            m_MovementCoroutine = StartCoroutine(MovementRoutine(input));
+        } else if (input.phase == InputActionPhase.Canceled)
+        {
+            if (m_MovementCoroutine != null)
+            {
+                StopCoroutine(m_MovementCoroutine);
+            }
+        }
+    }
+
+    private IEnumerator MovementRoutine(InputAction.CallbackContext input)
+    {
+        while (true)
+        {
+            Move(input.ReadValue<Vector2>());
+            yield return null;
+        }
+    }
+
+    private void Move(Vector2 movement)
+    {
+        movement *= m_Speed * Time.deltaTime;
+        Vector3 newPos = transform.localPosition;
+        newPos.x += movement.x;
+        newPos.z += movement.y;
+        transform.localPosition = newPos;
+    }
+}
