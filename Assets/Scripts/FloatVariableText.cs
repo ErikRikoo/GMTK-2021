8e3931581570c7b1c2a72a7c61a1006169152b75
jb106 <35401731+jb106@users.noreply.github.com>
2021-06-13 19:30:09 +0200

    hud basique et changement angle cam

---
+++ Assets/Scripts/FloatVariableText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+using UnityEngine.UI;
+
+public class FloatVariableText : MonoBehaviour
+{
+    [SerializeField] private FloatVariable _timer;
+    [SerializeField] private Text _text;
+
+    private void Update()
+    {
+        _text.text = ((int)_timer.Value).ToString();
+    }
+}
