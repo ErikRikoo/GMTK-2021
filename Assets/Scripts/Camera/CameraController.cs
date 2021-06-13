f1dc68317f0ee742d21f835bfdd7189da0174839
erik <erik@pristimantis.com>
2021-06-13 14:09:21 +0200

    Temp

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace UnityTemplateProjects.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform m_Target;
+        [SerializeField] private float m_MovementSpeed;
+
+        [SerializeField] private bool m_LookAtOnAwake;
+        [SerializeField] private Transform m_LookAtTarget;
+        
+
+        private void Awake()
+        {
+            transform.position = m_Target.position;
+            if (m_LookAtOnAwake)
+            {
+                transform.LookAt(m_LookAtTarget.position, transform.forward);
+            }
+        }
+
+        void LateUpdate()
+        {
+            UpdatePosition();
+        }
+
+        private void UpdatePosition()
+        {
+            Vector3 goal = m_Target.position;
+            
+            Vector3 nextPosition = Vector3.LerpUnclamped(transform.position, goal, m_MovementSpeed);
+            transform.position = nextPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/GoalVariableFollow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+namespace Camera
+{
+    public class GoalVariableFollow : MonoBehaviour
+    {
+        [SerializeField] private Vector2Variable m_Goal;
+        [SerializeField] private Vector3 m_Offset;
+
+        private void Awake()
+        {
+            m_Goal.Changed.Register(OnGoalChanged);
+            OnGoalChanged(m_Goal.Value);
+        }
+
+        private void OnGoalChanged(Vector2 newPos)
+        {
+            Vector3 temp = new Vector3(newPos.x, 0, newPos.y);
+            temp += m_Offset;
+            transform.position = temp;
+        }
+    }
+}
